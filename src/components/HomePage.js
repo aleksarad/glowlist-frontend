43629e53638b8f1cbb12e7683f56749d29b3b574
aleksarad <alexa.radevich1@gmail.com>
2020-11-19 17:21:11 -0500

    added smfooter speech buuble, includes my github link

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,7 +5,10 @@ export default function HomePage() {
     return (
         <div className="centered-container-horizontal">
             <span className="main-logo">glowlist</span>
-            <img src={jellyFish} alt="a pink, smiling, cartoon jellyfish" className="jellyfish"></img>
+            <div id="github-bubble-container">
+                <div className="github-bubble">made by <a href="https://github.com/aleksarad" target="_blank" rel="noopener noreferrer">aleksa</a> with 💗</div>
+                <img src={jellyFish} alt="a pink, smiling, cartoon jellyfish" className="jellyfish"></img>
+            </div>
         </div>
     )
 }
